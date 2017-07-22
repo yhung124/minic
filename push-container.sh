9c682e8be944699ee9212cf7d514a99c582a1ac5
Raymond <yhung124@gmail.com>
2017-07-22 08:48:56 +0000

    Modify path of push images

--- push-container.sh
+++ push-container.sh
@@ -3,11 +3,11 @@ set -e
 ROOT=$(dirname $(readlink -f "$0"))
 source ${ROOT}/config.sh
 
-ALL_CONTAINERS=$(cd ${ROOT}/containers && ls -d */)
+ALL_CONTAINERS=$(cd ${ROOT}/minic-container/containers && ls -d */)
 for con in ${ALL_CONTAINERS}; do
   echo -e "\nStart to push container image: ${con//\//} \n"
   pushd . >/dev/null 2>&1
-  cd ${ROOT}/containers/${con}
+  cd ${ROOT}/minic-container/containers/${con}
   ./push.sh
   popd >/dev/null 2>&1
   echo -e "Build container image ${con//\//} done\n"
